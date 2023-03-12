31c5fe2f106d0d7168994931231eb3f8122f2939
V-Amit Chawla <Amit.Chawla@blackbaud.me>
2023-03-12 00:01:01 -0500

    Delete redundant id server

--- YCompanyIdentityServer/YCompanyIdentityServer/Pages/Account/Logout/LogoutOptions.cs
+++
@@ -1,8 +0,0 @@
-
-namespace YCompanyIdentityServer.Pages.Logout;
-
-public class LogoutOptions
-{
-    public static bool ShowLogoutPrompt = true;
-    public static bool AutomaticRedirectAfterSignOut = false;
-}
