f65c31482c430bf0322d01f9a60d43758b4c92be
Shrey Gupta <shrey.gupta@nagarro.com>
2024-02-12 12:54:41 +0530

    CustomConfigurationProvider-Added

---
+++ CustomConfigurationProvider/SecurityMetaData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YCompany.Configurations
+{
+    public class SecurityMetaData
+    {
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+    }
+}
