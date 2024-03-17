519478ae22cea7ba4b0861430b95c26a5ebfc330
Shrey Gupta <shrey.gupta@nagarro.com>
2024-03-17 16:23:30 +0530

    Aws secrets Manager Added

--- CustomConfigProvider/CustomConfigurationProvider.cs
+++ CustomConfigProvider/CustomConfigurationProvider.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Text.Json;
+using CustomConfigProvider;
 using Microsoft.Extensions.Configuration;
 
 namespace YCompany.Configurations
 {
     public class CustomConfigurationProvider :ConfigurationProvider
     {
-        public override void Load()
+        public async Task<string> GetSecrets()
         {
+            var secretsService = new SecretsManagerService("AKIATODEJPR4HSRAM5UQ", "7u0tKwy46NWtjdzT3cu0v8J0Ym0EOuvDdRzMjgoj", "ap-south-1");
+            string secretValue = await secretsService.GetSecretValueByNameAsync("ApiAccessKeys");
+            return secretValue;
+        }
+        public async override void Load()
+        {
+           
             var text = File.ReadAllText(@"D:\SecurityMetadata.json");
             var options = new JsonSerializerOptions
             { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
